e02da75f3d3145567e2a886e0d05d607bf1d6bc1
hema <hema.gowda1991@gmail.com>
2019-07-28 07:47:06 +0530

    My first git commit
    s Initial commit

---
+++ Accept.java
@@ -0,0 +1,10 @@
+class SampleAccept
+{
+	public static void main(String args[])
+	{
+	   int n = Integer.parseInt(args[0]);
+	   String name = args[1];
+	   System.out.println(n);
+	System.out.println(name);
+	}
+}
---
+++ BreakDemo.java
@@ -0,0 +1,17 @@
+public class BreakDemo
+{
+	public static void main(String args[])
+	{
+		for(int i = 0; i<5; i++)
+		{
+			if(i == 3)
+			{
+				break;
+			}
+			
+			System.out.println("i = "+i);
+			System.out.println("git edit");
+		}
+	}
+}
+//Comment for merging
